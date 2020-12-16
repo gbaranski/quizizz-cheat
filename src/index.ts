3c8d9bd07972b256bae1fd70b502de95343c72ad
gbaranski <gbaranski19@gmail.com>
2020-12-16 21:55:04 +0100

    WIP: retreiving questions

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,69 @@
+import { ServerRequest, ServerResponse, VueElement } from "./types";
+
 console.log("Hi there!!");
 
-interface VueObject {}
+const sendResponse = async (request: ServerRequest): Promise<void> => {
+  const res = await fetch("https://game.quizizz.com/play-api/v4/proceedGame", {
+    method: "POST",
+    body: JSON.stringify(request),
+  });
+  console.log({ res });
+  console.log({ json: await res.json() });
+};
 
-interface VueElement extends Element {
-  __vue__: VueObject;
-}
+const getQuestionInfo = (): Promise<{
+  id: string;
+  roomHash: string;
+}> => {
+  const rootObject = document.querySelector("body > div") as VueElement | null;
+  if (!rootObject) throw new Error("Could not retreive root object");
+  const vue = rootObject.__vue__;
 
-function start() {
-  console.log("Running!");
-  const fn = () => {
-    // @ts-ignore
-    const vue = document.querySelector("body > div").__vue__;
-    console.log(vue);
+  return new Promise((resolve, reject) => {
+    // Non blocking loop
+    const interval = setInterval(() => {
+      const questionId = vue.$store._vm.currentQuestion.id;
+      const roomHash = vue.$store._vm._data.$$state.game.data.roomHash;
+      if (questionId && roomHash) {
+        resolve({
+          id: questionId,
+          roomHash,
+        });
+        clearInterval(interval);
+      }
+    }, 0);
+  });
+};
+
+async function start() {
+  console.log("Started!");
+  const questionInfo = await getQuestionInfo();
+  console.log("Successfully retreived question info!");
+
+  // @ts-ignore
+  window.getAnwsers = async () => {
+    const request: ServerRequest = {
+      gameType: "live",
+      playerId: Math.random().toString(16),
+      powerupEffects: {
+        destroy: [],
+      },
+      questionId: questionInfo.id,
+      response: {
+        attempt: 0,
+        isEvaluated: false,
+        //   @ts-ignore just for testing
+        provisional: undefined,
+        questionId: questionInfo.id,
+        questionType: "MSQ",
+        response: [0, 3],
+        responseType: "original",
+        timeTaken: Math.floor(Math.random() * 10000),
+      },
+      roomHash: questionInfo.roomHash,
+    };
+    sendResponse(request);
   };
-  setInterval(fn, 500);
 }
 
 const script: HTMLScriptElement = document.createElement("script");

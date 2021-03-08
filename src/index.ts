8eda526ad9b45d9648a4fc54f00e17307892d6d9
gbaranski <root@gbaranski.com>
2021-03-08 16:18:52 +0100

    fix: change discord name

--- src/index.ts
+++ src/index.ts
@@ -82,7 +82,7 @@ const getRoomHash = (): string => {
 }
 
 const msg = `%c 
-    Script created by grzegorz#5119! 
+    Script created by gbaranski#5119! 
     https://github.com/gbaranski/quizizz-cheat
       `;
 
